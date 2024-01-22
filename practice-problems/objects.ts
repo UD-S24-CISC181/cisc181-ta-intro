cf8eb921db814ad9f42ac677860e4ceb93d408b9
Faith Lovell <faithlxvell@gmail.com>
2024-01-22 13:20:17 -0500

    objects problems

---
+++ practice-problems/objects.ts
@@ -0,0 +1,36 @@
+/**
+ * Practice with objects/ interfaces
+ * See: https://www.typescriptlang.org/docs/handbook/2/objects.html
+ */
+
+/**
+ * Create an interface called Person that has the following properties:
+ * firstName (a string)
+ * lastName (a string)
+ * age (a number)
+ */
+
+
+
+
+/**
+ * Create a person object called person1 and fill in the properties with your information.
+ */
+
+
+
+
+/**
+ * Create an interface called CandyJar that has the following properties:
+ * chocolates (a number)
+ * gummies (a number)
+ * lollipops (a number)
+ * owner (a Person)
+ */
+
+
+
+
+/**
+ * Create a function that takes in a CandyJar and returns the total number of candies in the jar.
+ */
