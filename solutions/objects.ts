0a7bbc9bc27e6f7cb9a092acd553b3be4eda0ac6
Faith Lovell <faithlxvell@gmail.com>
2024-01-22 13:16:24 -0500

    a bit on objects

---
+++ solutions/objects.ts
@@ -0,0 +1,46 @@
+/**
+ * Practice with objects/ interfaces
+ * See: https://www.typescriptlang.org/docs/handbook/2/objects.html
+ */
+
+/**
+ * Create an interface called Person that has the following properties:
+ * firstName (a string)
+ * lastName (a string)
+ * age (a number)
+ */
+interface Person {
+  firstName: string;
+  lastName: string;
+  age: number;
+}
+
+/**
+ * Create a person object called person1 and fill in the properties with your information.
+ */
+let person1: Person = {
+  firstName: "Faith",
+  lastName: "Lovell",
+  age: 20,
+};
+
+/**
+ * Create an interface called CandyJar that has the following properties:
+ * chocolates (a number)
+ * gummies (a number)
+ * lollipops (a number)
+ * owner (a Person)
+ */
+interface CandyJar {
+  chocolates: number;
+  gummies: number;
+  lollipops: number;
+  owner: Person;
+}
+
+/**
+ * Create a function that takes in a CandyJar and returns the total number of candies in the jar.
+ */
+function totalCandies(candyJar: CandyJar): number {
+  return candyJar.chocolates + candyJar.gummies + candyJar.lollipops;
+}
