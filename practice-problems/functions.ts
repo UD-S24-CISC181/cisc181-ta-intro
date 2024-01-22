d314fce31a0195f38d1143947bdd684e12d92a4f
Faith Lovell <faithlxvell@gmail.com>
2024-01-22 13:22:07 -0500

    functions problems

---
+++ practice-problems/functions.ts
@@ -0,0 +1,95 @@
+//Practice writing simple functions in TypeScript
+
+//basics
+
+/**
+ * Computes and returns the area of the triangle given its base and height.
+ */
+
+
+
+
+/**
+ * Consumes three numbers and produces their sum. If a number is negative, ignore it.
+ */
+
+
+
+
+
+/**
+ * Returns true if the given number is a multiple of 3 or 7, false otherwise.
+ */
+
+
+
+
+//loops
+
+/**
+ * Multiplies two numbers together using for loop addition
+ */
+
+
+
+
+/**
+ * Adds elements of an array using a for..of statement
+ * Read about iterators here: https://www.typescriptlang.org/docs/handbook/iterators-and-generators.html
+ */
+
+
+
+
+//arrays
+
+/**
+ * Filters out all failing grades from a list of grades, assume failing grade is anything below a 70
+ * Use the filter method to solve this problem!
+ */
+
+
+
+
+/**
+ * Given a list of grades, adjust the grades if the last grade is less than 95.
+ * Return new array with an extra 5 points to the last grade in the array if the grades need to be adjusted.
+ * If the grades do not need to be adjusted, return a copy of the original array.
+ * Do not use a loop to solve this problem!
+ */
+
+
+
+
+/**
+ * Consumes an array of colors (e.g., 'red', 'purple') and returns true if ALL
+ * the colors are either 'red', 'blue', or 'green'. If an empty list is given,
+ * then return true.
+ */
+
+
+
+
+//strings
+
+/**
+ * Consumes a string and a keyword returns whether or not the string contains that word.
+ * If the string is empty, return null.
+ */
+
+
+
+
+/**
+ * Given a string, return a new string where "not " has been added to the
+ * front. However, if the string already begins with "not", return the
+ * string unchanged.
+ */
+
+
+
+
+/**
+ * Given a string, return a new string where the first and last characters
+ * have been exchanged.
+ */
